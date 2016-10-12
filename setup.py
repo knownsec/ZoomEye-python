a31b77db20f457f4437851140a116c2851ed4911
nixawk <hap.ddup@gmail.com>
2016-10-12 00:18:18 -0500

    add setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+from distutils.core import setup
+
+setup(name='zoomeye',
+      version='1.0.0',
+      url="https://www.zoomeye.org/api/doc",
+      description='ZoomEye is a search engine for cyberspace that lets the user find specific network components(ip, services, etc.).',
+      long_description=open('README.md').read(),
+      author='Zoomeye Team',
+      maintainer='Nixawk',
+      author_email='team@zoomeye.org',
+      py_modules=['zoomeye'],
+      license='GPL-2.0',
+
+      install_requires=[
+          'requests',
+          'getpass',
+      ]
+     )
